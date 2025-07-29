a93bf1e2427eea0a7561afbff30c68465c61fc8c
kabilanpersonal <kabilan0904@gmail.com>
2025-07-29 14:10:27 +0530

    palindrome

--- Strings/palindrome.js
+++ Strings/palindrome.js
@@ -1,11 +1,16 @@
 const numbers = [121, 343, 123, 456, 989, 1001, 234];
 
 const palindromes = numbers.filter(num => {
-  const str = num.toString();
-  return str === str.split('').reverse().join('');
+  return num == num.toString().split('').reverse().join('');
 });
 
 const nonPalindromes = numbers.filter(num => !palindromes.includes(num));
 
 console.log(palindromes);       // [121, 343, 989, 1001]
 console.log(nonPalindromes);    // [123, 456, 234]
+
+//sol-2
+function checkPalindrom(a){
+  return a.split('').reverse().join('') === a
+}
+console.log("Palindrome : ",checkPalindrom("madam"))
